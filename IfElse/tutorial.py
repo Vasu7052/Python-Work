966352fb90a21e7a7c3112549232d60e5593edb3
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 16:51:15 +0530

    Python Work

---
+++ IfElse/tutorial.py
@@ -0,0 +1,22 @@
+print("I hope this works")
+
+age = 20
+
+if age is 20 :
+    print("OK OK")
+elif age > 18 :
+    print("YEAH")
+    print("GOOD")
+else:
+    print("NO")
+    print("GOOD")
+
+name = "Vasu"
+if name is "Vasu":
+    print("Hey there Vasu")
+elif name is "Lucy":
+    print("What up Lucedawg")
+elif name is "Sammy":
+    print("What up Slammy")
+else:
+    print("Please sign up for the site!")
---
+++ Loops/tutorial.py
@@ -0,0 +1,18 @@
+fruits = ["Apple" , "Grapes" , "Watermelon" , "Mango"]
+
+for f in fruits :
+    print(f)
+    print(len(f))
+
+print("========================")
+
+for f in fruits[:3] :
+    print(f)
+    print(len(f))
+
+print("========================")
+
+for x in range(5) :
+    print("Range Demo")
+
+print("========================")
