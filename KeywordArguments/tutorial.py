bb74236b6fdaff750ff3216cf2ecfd45e4b7b51d
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 17:17:45 +0530

    Python Work

---
+++ KeywordArguments/tutorial.py
@@ -0,0 +1,8 @@
+
+def func(name="Vasu" , action="watched" , item="tv") :
+    print(name,action,item)
+
+func()
+func("I" , "rode" , "Bike")
+func(action="switched")
+func(action="fill" , item="bottle")
