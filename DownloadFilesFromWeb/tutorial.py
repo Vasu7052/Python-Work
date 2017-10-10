8e941d7edf667ed875c8a32ef75a55286945ecc4
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 18:18:23 +0530

    Python Work

--- DownloadFilesFromWeb/tutorial.py
+++ DownloadFilesFromWeb/tutorial.py
@@ -6,8 +6,10 @@ def downloadFile(url) :
     response = urllib.request.urlopen(url) # connection is stored in response
     csv = response.read()
     csv_str = str(csv)
-    fw = open("Data.csv" , "w")
-    fw.write(csv_str)
+    fw = open("Data.csv", "w")
+    array = csv_str.split("\\n")
+    for f in array :
+        fw.write(f + "\n")
     fw.close()
 
 downloadFile(url)
