51d57f084383846614acac94ef68bffdc5b9bee1
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 19:02:39 +0530

    Python Work

--- WebCrawler/tutorial.py
+++ WebCrawler/tutorial.py
@@ -3,13 +3,13 @@ from bs4 import BeautifulSoup
 
 url = "https://thenewboston.com/search.php"
 
-def trade_spide(max_pages) :
+def trade_spider1(max_pages) :
     page = 1
     while page <= max_pages :
         tempUrl = url + "?page=" + str(page)
         sourceCode = requests.get(tempUrl)
-        plainText = sourceCode.text
-        soup = BeautifulSoup(plainText)
+        plain_text = sourceCode.text.encode('ascii', 'replace')
+        soup = BeautifulSoup(plain_text, 'html.parser')
         for f in soup.findAll("a",{"class" : "user-name"}) :
             href = f.get("href")
             title = f.string
@@ -17,5 +17,16 @@ def trade_spide(max_pages) :
         page += 1
 
 
+def get_single_item(url) :
+    sourceCode = requests.get(url)
+    plain_text = sourceCode.text.encode('ascii', 'replace')
+    soup = BeautifulSoup(plain_text, 'html.parser')
+    for item_name in soup.findAll("span",{"class" : "text-semibold"}) :
+        print(item_name.string)
+    print("All Links : ")
+    for item_name in soup.findAll("a") :
+        print(item_name.get("href"))
 
-trade_spide(1)
+# trade_spider1(2)
+
+get_single_item("https://thenewboston.com/profile.php?user=522")
