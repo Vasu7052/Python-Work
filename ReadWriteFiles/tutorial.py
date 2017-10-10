c84602f21b999403c9da4ffe3607b03ff2ef9972
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 18:05:38 +0530

    Python Work

--- ReadWriteFiles/tutorial.py
+++ ReadWriteFiles/tutorial.py
@@ -2,3 +2,8 @@ fw = open("sample.txt" , "w" )
 fw.write("I am writing some text in my files\n")
 fw.write("Yes its working")
 fw.close()
+
+fr = open('sample.txt', 'r')
+text = fr.read()
+print(text)
+fr.close()
