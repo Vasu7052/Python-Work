10125d92003a8a6329892381d3b19ec7946b1dbb
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 15:51:43 +0530

    Python Work

---
+++ First.py
@@ -0,0 +1,74 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 17:26:49) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 3/3
+1.0
+>>> 5/2.5
+2.0
+>>> 7/3
+2.3333333333333335
+>>> 3*5
+15
+>>> clr
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    clr
+NameError: name 'clr' is not defined
+>>> clear
+Traceback (most recent call last):
+  File "<pyshell#5>", line 1, in <module>
+    clear
+NameError: name 'clear' is not defined
+>>> clrscr
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    clrscr
+NameError: name 'clrscr' is not defined
+>>> 8+2*10
+28
+>>> 18/4
+4.5
+>>> 18//4
+4
+>>> 7//3
+2
+>>> 2%1
+0
+>>> 5%2
+1
+>>> 5**3
+125
+>>> num = 5.0
+>>> str = "Hey"
+>>> str = HEY
+Traceback (most recent call last):
+  File "<pyshell#16>", line 1, in <module>
+    str = HEY
+NameError: name 'HEY' is not defined
+>>> num2 = 2
+>>> num2/num
+0.4
+>>> str = 'HEY'
+>>> str
+'HEY'
+>>> num2
+2
+>>> "HEYA"
+'HEYA'
+>>> print("jsdbvkjdbsv")
+jsdbvkjdbsv
+>>> print('c:\k\bdbf')
+c:\kdbf
+>>> print(r"\n")
+\n
+>>> str = "Vasu"
+>>> str2 = "Gupta"
+>>> print(str+str2)
+VasuGupta
+>>> str+str2
+'VasuGupta'
+>>> str+=str2
+>>> str
+'VasuGupta'
+>>> str*5
+'VasuGuptaVasuGuptaVasuGuptaVasuGuptaVasuGupta'
+>>> 
