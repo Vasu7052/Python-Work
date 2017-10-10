e879d6e72edc0cc74e3e4d6052d8c66ab088d1ff
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 17:10:31 +0530

    Python Work

---
+++ DefaultValuesArguments/tutorial.py
@@ -0,0 +1,10 @@
+def getType(type="Unkown") :
+    if type is "m" :
+        type = "male"
+    elif type is "f" :
+        type = "female"
+    print(type)
+
+getType("m")
+getType("f")
+getType()
---
+++ Functions/tutorial.py
@@ -0,0 +1,14 @@
+def fruit1() :
+    print("Empty one")
+
+def fruit2(str) :
+    print(str)
+
+def fruit3(str) :
+    return str + " A fruit"
+
+fruit1()
+
+fruit2("Mango")
+
+print(fruit3("Grapes"))
