937043f08cfb44715213c017d8fc9da9a6d36c28
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 17:01:07 +0530

    Python Work

--- BreakContinue/tutorial.py
+++ BreakContinue/tutorial.py
@@ -1,7 +1,17 @@
 
 for x in range(5) :
     if x is 2 :
-        print(x  , " is the required number" , " Yeah")
+        print(x  , "is the required number" , "Yeah")
         break
+    else:
+        print(x)
+
+print("=========================")
+
+for x in range(5) :
+    if x is 2 :
+        print(x  , "is the required number" , "Yeah")
+        continue
+        print("This will not show")
     else:
         print(x)
