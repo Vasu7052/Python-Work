83fa5a6671bf9973c3a1c1a3a041e3238bb5ba8d
Vasu Gupta <androfly.official@gmail.com>
2017-10-10 17:27:53 +0530

    Python Work

--- IfElse/tutorial.py
+++ IfElse/tutorial.py
@@ -11,6 +11,8 @@ else:
     print("NO")
     print("GOOD")
 
+print("===============================")
+
 name = "Vasu"
 if name is "Vasu":
     print("Hey there Vasu")
@@ -20,3 +22,13 @@ elif name is "Sammy":
     print("What up Slammy")
 else:
     print("Please sign up for the site!")
+
+print("===============================")
+
+groceries = {'cereal', 'milk', 'starcrunch', 'beer', 'duck tape', 'lotion', 'beer'}
+print(groceries)
+
+if 'milk' in groceries:
+  print("You already have milk hoss!")
+else:
+  print("Oh yea, you need milk!")
